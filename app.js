06acdd8915d6dbf3adae40f8f68546cbe436493c
panther4446 <67631928+panther4446@users.noreply.github.com>
2023-10-05 03:33:05 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+// function time(date, day, time, GMT){
+// console.log("sat Dec 2015 22:18:39 GMT+0500 (PKR)")
+// }
+// time()
+
+// function name ( firstname, lastname){
+//     console.log("hasan sidiqui")
+// }
+// name()
+
+
+// num1 = prompt("enter your first number");
+// num2 = prompt("enter your second number");
+
+// function sum(num1 ,  num2){
+//     console.log(num1 + num2)
+    
+// }
+//     sum(num1 + num2)
+
+
+// function popUp() {
+//     alert("Hello World");
+//   }
+  
+//   for (var i = 0; i <= 10; i++) {
+//     popUp();
+//   }
+// function sum(x, y) {
+//       parameters
+//       console.log(x + y);
+//     }
+    
+
+// var num1 = +prompt("Enter first value");
+// var num2 = +prompt("Enter second value");
+
+//  (num1 , num2);
+
+// function multiplicationTable(num, length) {
+//   for (var i = 1; i <= length; i++) {
+//     document.write(num + "x" + i + "=" + num * i + "<br>");
+//   }
+// }
+
+
+
+// function calc(num1, opr, num2) {
+//   if (opr === "+") {
+//     return num1 + num2;
+//   } else if (opr === "-") {
+//     return num1 - num2;
+//   } else if (opr === "*") {
+//     return num1 * num2;
+//   } else if (opr === "/") {
+//     return num1 / num2;
+//   }
+// }
+
+// var num1 = +prompt("Enter first value");
+// var operator = prompt("Enter operator");
+// var num2 = +prompt("Enter second value");
+
+// var result = calc(num1, operator, num2);
+
+// console.log(result);
